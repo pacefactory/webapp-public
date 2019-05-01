4c37c99a5478f62822e37845f242747fb03a2dd8
Michael Polyak <mpolyak@gmail.com>
2019-05-01 16:16:17 -0400

    Updates

---
+++ precache-manifest.f28e8b152e98aa054fbe764acf8646f0.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "3334b6f0efa2f0b869f5",
+    "url": "/webapp-public/static/js/main.ca2945f0.chunk.js"
+  },
+  {
+    "revision": "9db66bc3633f5a3ad4fb",
+    "url": "/webapp-public/static/js/runtime~main.599221f3.js"
+  },
+  {
+    "revision": "eb61964ad49c2a1c9b90",
+    "url": "/webapp-public/static/js/2.49d6537b.chunk.js"
+  },
+  {
+    "revision": "0b7a4fcc0e1fd81a6fa19d27b7b20e92",
+    "url": "/webapp-public/static/media/schema.0b7a4fcc.graphql"
+  },
+  {
+    "revision": "c591572ac6d0ea5c9613e817b50cf44b",
+    "url": "/webapp-public/index.html"
+  }
+];
